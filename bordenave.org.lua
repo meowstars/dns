2f22a732612db3b61e183ab2f5b1e9e84e69b51f
Laetitia 'Meow' Bordenave <meow@meo.wf>
2019-03-13 17:38:45 +0100

    Configure bordenave.org

---
+++ bordenave.org.lua
@@ -0,0 +1,14 @@
+-- File: bordenave.org.lua
+-- Zone: bordenave.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "bordenave.org"
+
+-- A records
+a(_a, "51.15.146.222")
+
+-- CNAME records
+
+-- TXT records
+
+fastmail(_a)
