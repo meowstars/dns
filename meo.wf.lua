8a15d71acfc57976cf7de05460e941fe5e9826f8
Laetitia 'Meow' Bordenave <meow@meo.wf>
2019-03-13 16:57:21 +0100

    Configure meo.wf

---
+++ meo.wf.lua
@@ -0,0 +1,26 @@
+-- File: meo.wf.lua
+-- Zone: meo.wf
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "meo.wf"
+
+-- A records
+a(_a, "51.15.146.222")
+
+-- Physical locations
+a("merignac", "86.213.179.185")
+
+-- Servers
+a("scatcat", "51.15.146.222")
+a("azrael", "86.213.179.185")
+
+-- CNAME records
+cname("www", "scatcat.meo.wf")
+cname("wiki", "scatcat.meo.wf")
+
+-- CAA
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto:postmaster@meo.wf", "iodef")
+
+
+fastmail(_a)
