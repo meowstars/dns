5f847b458859c05f3966125b50b683a33e3c8bb8
Laetitia 'Meow' Bordenave <meow@meo.wf>
2019-03-13 16:46:45 +0100

    Configure meowstars.org

---
+++ meowstars.org.lua
@@ -0,0 +1,14 @@
+-- File: meowstars.org.lua
+-- Zone: meowstars.org
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "meowstars.org"
+
+-- A records
+a(_a, "51.15.146.222")
+
+-- CNAME records
+
+-- TXT records
+
+fastmail(_a)
