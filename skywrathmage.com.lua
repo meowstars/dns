841e2302640e37133d35bbbbb87af25b5ef67f70
Laetitia 'Meow' Bordenave <meow@meo.wf>
2019-03-13 16:15:42 +0100

    Initial commit, configure skywrathmage.com

---
+++ skywrathmage.com.lua
@@ -0,0 +1,16 @@
+-- File: skywrathmage.com.lua
+-- Zone: skywrathmage.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "skywrathmage.com"
+
+-- A records
+a(_a, "51.15.146.222")
+
+-- CNAME records
+cname("www", "scatcat.meo.wf")
+
+-- TXT records
+txt("_keybase.skywrathmage.com", "keybase-site-verification=DEwDHYCN2owa6UGIaiHhK2tnomQRSXI5erRnNZPeUng")
+
+fastmail(_a)
---
+++ templates/fastmail.lua
@@ -0,0 +1,49 @@
+function fastmail(domain)
+  -- Allows you to receive email at standard addresses, e.g. user@skywrathmage.com
+  mx(domain, "in1-smtp.messagingengine.com", 10)
+  mx(domain, "in2-smtp.messagingengine.com", 20)
+
+  -- Allows you to receive email at subdomain addresses, e.g. foo@user.skywrathmage.com
+  mx(concat("*", domain), "in1-smtp.messagingengine.com", 10)
+  mx(concat("*", domain), "in2-smtp.messagingengine.com", 20)
+
+  -- Allows you to log in to your account at http://mail.skywrathmage.com.
+  a(concat("mail", domain), "66.111.4.147")
+  a(concat("mail", domain), "66.111.4.148")
+
+  -- An 'A' record hides the wildcard subdomain MX record. This overrides that
+  -- to allow receiving email addressed to foo@mail.skywrathmage.com
+  mx(concat("mail", domain), "in1-smtp.messagingengine.com", 10)
+  mx(concat("mail", domain), "in2-smtp.messagingengine.com", 20)
+
+  -- DKIM: Allows us to sign the mail you send so receivers can verify it's
+  -- from you. This is important to ensure your message is not classified as
+  -- spam.
+  cname(concat("mesmtp._domainkey", domain), "mesmtp." .. domain .. ".dkim.fmhosted.com")
+  cname(concat("fm1._domainkey", domain), "fm1." .. domain .. ".dkim.fmhosted.com")
+  cname(concat("fm2._domainkey", domain), "fm2." .. domain .. ".dkim.fmhosted.com")
+  cname(concat("fm3._domainkey", domain), "fm3." .. domain .. ".dkim.fmhosted.com")
+
+  -- Configure SPF: Allows receivers to know you send your mail via FastMail,
+  -- and other servers.
+  txt(domain, "v=spf1 include:spf.messagingengine.com ?all")
+  txt(concat("_dmarc", domain), '"v=DMARC1; p=none; adkim=s; aspf=r; fo=1; rua=mailto:postmaster@' .. domain .. '"')
+
+  -- SRV: Allows email clients to automatically find the correct settings for
+  -- your account.
+  srv(concat("_submission._tcp", domain), "smtp.fastmail.com", 587, 0, 1)
+  srv(concat("_imap._tcp", domain), ".", 0, 0, 0)
+  srv(concat("_imaps._tcp", domain), "imap.fastmail.com", 993, 0, 1)
+  srv(concat("_pop3._tcp", domain), ".", 0, 0, 0)
+  srv(concat("_pop3s._tcp", domain), "pop.fastmail.com", 995, 0, 1)
+
+  -- SRV: Allows CardDAV clients to automatically find the correct settings for
+  -- your account.
+  srv(concat("_carddav._tcp", domain), ".", 0, 0, 0)
+  srv(concat("_carddavs._tcp", domain), "carddav.fastmail.com", 443, 0, 1)
+
+  -- SRV: Allows CalDAV clients to automatically find the correct settings for
+  -- your account.
+  srv(concat("_caldav._tcp", domain), ".", 0, 0, 0)
+  srv(concat("_caldavs._tcp", domain), "caldav.fastmail.com", 443, 0, 1)
+end
